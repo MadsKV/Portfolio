2808b0de1341dc1f50dc704b8fa5baff1d482631
Mads Klausen <Mads.v.k@hotmail.com>
2024-11-07 02:05:02 +0100

    Multiple components added for future reference. Aswell as configurered Vue.Router

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import About from './../components/About.vue'
+import Contact from './../components/Contact.vue'
+import Experience from './../components/Experience.vue'
+import Footer from './../components/Footer.vue'
+import Header from './../components/Header.vue'
+import Navbar from './../components/Navbar.vue'
+import Projects from './../components/Projects.vue'
+import HomePage from './../components/HomePage.vue'
+
+const routes = [
+  { path: '/', component: HomePage },
+  { path: '/About', component: About },
+  { path: '/Contact', component: Contact },
+  { path: '/Experience', component: Experience },
+  { path: '/Footer', component: Footer },
+  { path: '/Header', component: Header },
+  { path: '/Navbar', component: Navbar },
+  { path: '/Projects', component: Projects },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
